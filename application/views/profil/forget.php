4a5105aa0073dcc52841685f1a9ab25f64f67b01
Indri Junanda <ind.junanda@gmail.com>
2020-06-03 14:32:55 +0700

    dikit

--- application/views/profil/forget.php
+++ application/views/profil/forget.php
@@ -13,11 +13,7 @@
             </div>
             <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">Ubah Password</button>
             <div class="row m-t-20 m-b--5 align-center">
-<<<<<<< HEAD:application/views/profil/forget.php
                 <a href="{{site_url('profil')}}">Batal</a>
-=======
-            <a href="<?= site_url('auth') ?>" id="to-recover" type="button"><i class="fa fa-lock m-r-5"></i> Sign in!</a>
->>>>>>> 9e0c757f3962d42d7d5ac0eb623dd40ffd509104:application/views/auth/forgotpassword.php
             </div>
         </form>
     </div>
