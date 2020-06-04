571e7187dee7e523bf769fa31b777bcaf816ca20
Dadan <dhika.d.addorio@gmail.com>
2020-06-04 15:39:40 +0700

    export

--- application/views/admin/usermanagement/export.php
+++ application/views/admin/usermanagement/export.php
@@ -15,9 +15,27 @@ table, th, td { border: 1px solid black; }
 </style>
 <body>
     <div class="container page">
-        <h3 style="text-align:center;">DAFTAR MEDIA MASSA YANG MELAKUKAN KERJA SAMA DENGAN <br> PEMERINTAH KABUPATEN BINTAN</h3>
-        <br><br><br>
-        <table id="table" style="font-size:12px;border-collapse:collapse;width:100%;">
+        <table style="border-collapse:collapse;border:none;width:100%;margin:0;padding:0;">
+            <tr>
+                <td rowspan="4" style="text-align:right;border:none;width:25%;"><img src="assets/images/bintan.png" width=100px;></td>
+                <td style="text-align:center;font-size:20px;border:none;font-weight:bold;width:50%;">PEMERINTAH KABUPATEN BINTAN</td>
+                <td rowspan="4" style="text-align:left;border:none;width:25%;"><img src="assets/images/kominfo.png" width=100px;></td>
+            </tr>
+            <tr>
+                <td style="text-align:center;font-size:24px;border:none;font-weight:bold;">DINAS KOMUNIKASI DAN INFORMATIKA</td>
+            </tr>
+            <tr>
+                <td style="text-align:center;font-size:16px;border:none;">Jl. Raya Tanjungpinang -  Tanjung Uban Km. 42</td>
+            </tr>
+            <tr>
+                <td style="text-align:center;font-size:16px;border:none;">Email: Kominfo@bintankab.go.id</td>
+            </tr>
+        </table>
+        <hr>
+        <br>
+        <h4 style="text-align:center;">DAFTAR MEDIA MASSA YANG MELAKUKAN KERJA SAMA DENGAN <br> PEMERINTAH KABUPATEN BINTAN</h4>
+        <br>
+        <table id="table" style="font-size:13px;border-collapse:collapse;width:100%;">
             <thead>
                 <tr>
                     <th style="padding:3;height:30;">No</th>
@@ -36,10 +54,10 @@ table, th, td { border: 1px solid black; }
             <?php foreach($mediamassa as $media){ ?>
                 <tr>
                     <td style="text-align:center;padding:3;"><?= $no++; ?></td>
-                    <td style="padding:3;"><?= $media->nama ?></td>
-                    <td style="padding:3;"><?= $media->perusahaan ?></td>
-                    <td style="padding:3;"><?= $media->pemimpin ?></td>
-                    <td style="padding:3;"><?= $media->kabiro ?></td>
+                    <td style="padding:3;word-wrap: break-word;"><?= $media->nama ?></td>
+                    <td style="padding:3;word-wrap: break-word;"><?= $media->perusahaan ?></td>
+                    <td style="padding:3;word-wrap: break-word;"><?= $media->pemimpin ?></td>
+                    <td style="padding:3;word-wrap: break-word;"><?= $media->kabiro ?></td>
                     <td style="padding:3;text-align:center;"><?= ucfirst($media->tipe_publikasi) ?></td>
                     <td style="padding:3;text-align:center;"><?= ucwords(remo($media->tipe_media_massa)) ?></td>
                     
