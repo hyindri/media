21bcbda29893d1b84a7aa641da225197f4869c12
Hendra Riadi <hendrar9@gmail.com>
2020-06-07 14:03:42 +0700

    Nambah toastr jika status akun masih 'registrasi'

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -13,7 +13,7 @@ class Dashboard extends CI_Controller
 			$this->session->set_flashdata('msg', '<div class="alert alert-danger"><strong>Oops!</strong> Mohon Login Dahulu </div>');
 			redirect(site_url(''));
 		}
-		$this->load->model('Medmas_model','medmas');
+		$this->load->model('Medmas_model', 'medmas');
 	}
 
 	function index()
@@ -74,44 +74,55 @@ class Dashboard extends CI_Controller
 	public function userprofile($id)
 	{
 		$row = $this->medmas->get_by_id($id);
-		$data = array (
-            'username' => $this->session->userdata('username'),
-            'level' => $this->session->userdata('level'),
-            'status' => $this->session->userdata('status'),
-            'nama' => $row->nama,
-            'tipe_mediamassa' => $row->tipe_media_massa,
-            'tipe_publikasi' => $row->tipe_publikasi,
-            'status' => $this->session->userdata('status'),
-            'pimpinan' => $row->pimpinan,            
-            'npwp' => $row->npwp,
-            'mulai_mou' => date('d/m/Y', strtotime($row->mulai_mou)),
-            'akhir_mou' => date('d/m/Y',strtotime($row->akhir_mou)),
-            'perusahaan' => $row->perusahaan,
-            'alamat_per' => $row->alamat,
-            'rekening' => $row->rekening,
-            'kabiro' => $row->kabiro,
-            'surat_kabiro' => $row->surat_kabiro,
-            'telp' => $row->no_telp,
-            'wartawan' => $row->wartawan,
-            'sertifikat' => $row->sertifikat_uji,
-            'verifikasi' => $row->verifikasi_pers,
-            'penawaran_kerjasama' => $row->penawaran_kerja_sama
-            
-        );      
-		view('profil.index',$data);
+		$data = array(
+			'username' => $this->session->userdata('username'),
+			'level' => $this->session->userdata('level'),
+			'status' => $this->session->userdata('status'),
+			'nama' => $row->nama,
+			'tipe_mediamassa' => $row->tipe_media_massa,
+			'tipe_publikasi' => $row->tipe_publikasi,
+			'status' => $this->session->userdata('status'),
+			'pimpinan' => $row->pimpinan,
+			'npwp' => $row->npwp,
+			'mulai_mou' => date('d/m/Y', strtotime($row->mulai_mou)),
+			'akhir_mou' => date('d/m/Y', strtotime($row->akhir_mou)),
+			'perusahaan' => $row->perusahaan,
+			'alamat_per' => $row->alamat,
+			'rekening' => $row->rekening,
+			'kabiro' => $row->kabiro,
+			'surat_kabiro' => $row->surat_kabiro,
+			'telp' => $row->no_telp,
+			'wartawan' => $row->wartawan,
+			'sertifikat' => $row->sertifikat_uji,
+			'verifikasi' => $row->verifikasi_pers,
+			'penawaran_kerjasama' => $row->penawaran_kerja_sama
+
+		);
+		view('profil.index', $data);
 	}
-	
+
 	function set_status_notif()
 	{
-        if ($this->session->userdata('level') == 'superadmin' || $this->session->userdata('level') == 'admin' ) {
+		if ($this->session->userdata('level') == 'superadmin' || $this->session->userdata('level') == 'admin') {
 			$id = $this->session->userdata('id_user');
-		}else{
+		} else {
 			$id = $this->session->userdata('id_user');
 		}
-		$data = array (
+		$data = array(
 			'dibaca' => '2'
 		);
-		return $this->notifikasi->ubah_dibaca($id,$data);
+		return $this->notifikasi->ubah_dibaca($id, $data);
 		echo json_encode($data);
+	}
+
+	function get_status_akun()
+	{
+		$output = array();
+		$data = $this->dashboard->__get_statusAkun()->result();
+		foreach ($data as $row) {
+			$output['username'] = $row->username;
+			$output['status'] = $row->status;
+		}
+		echo json_encode($output);
 	}
 }
